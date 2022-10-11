ba118c468e6b3b06ba97ff897048e4f593ef1db8
cyace84 <cyace84@gmail.com>
2022-10-11 17:30:33 +0200

    worked tests

--- tests/unified_staking_pool.py
+++ tests/unified_staking_pool.py
@@ -501,8 +501,8 @@ def test_vesting_incentive():
     )
     scenario.verify(staking_pool.data.max_release_period == 360 * 24 * 60 * 60)
 
-@sp.add_test(name="Vesting Staking Pool multistakes")
-def test_multi_stakes():
+@sp.add_test(name="Vesting Staking Pool multistakes solo")
+def test_multi_stakes_solo():
     scenario = sp.test_scenario()
     scenario.h1("Unified Staking Pool Test")
     scenario.table_of_contents()
@@ -748,3 +748,201 @@ def test_multi_stakes():
     scenario.verify_equal(
         reward_token.data.ledger[unified_staking_pool_key], remaining_reward
     )
+
+@sp.add_test(name="Vesting Staking Pool multistakes multi")
+def test_multi_stakes():
+    scenario = sp.test_scenario()
+    scenario.h1("Staking Pool Unit Test")
+    scenario.table_of_contents()
+
+    scenario.h2("Bootstrapping")
+    token_id = sp.nat(0)
+
+    administrator = sp.test_account("Administrator")
+    alice = sp.test_account("Alice")
+    bob = sp.test_account("Robert")
+    dan = sp.test_account("Dan")
+
+    scenario.show([administrator, alice, bob, dan])
+
+    reward_token = DummyFA2({fa2.LedgerKey.make(0, administrator.address): sp.unit})
+    staking_token = DummyFA2({fa2.LedgerKey.make(0, administrator.address): sp.unit})
+
+    scenario += reward_token
+    scenario += staking_token
+
+    scenario += reward_token.set_token_metadata(
+        sp.record(token_id=token_id, token_info=sp.map())
+    ).run(sender=administrator)
+
+    scenario += staking_token.set_token_metadata(
+        sp.record(token_id=token_id, token_info=sp.map())
+    ).run(sender=administrator)
+
+    scenario.h1("Long Staking with release of 0")
+    staking_pool = staking_pool = UnifiedStakingPool(sp.record(id=token_id, address=staking_token.address), True, sp.record(id=0, address=reward_token.address), 180 * 24 * 60 * 60, {administrator.address: 1})
+
+    scenario += staking_pool
+
+    scenario += staking_token.mint(
+        owner=alice.address,
+        token_id=token_id,
+        token_amount=10 * Constants.PRECISION_FACTOR,
+    )
+    scenario += staking_token.mint(
+        owner=bob.address,
+        token_id=token_id,
+        token_amount=10 * Constants.PRECISION_FACTOR,
+    )
+    scenario += staking_token.mint(
+        owner=dan.address,
+        token_id=token_id,
+        token_amount=10 * Constants.PRECISION_FACTOR,
+    )
+    scenario += staking_token.update_operators(
+        [
+            sp.variant(
+                "add_operator",
+                sp.record(
+                    owner=alice.address,
+                    operator=staking_pool.address,
+                    token_id=token_id,
+                ),
+            )
+        ]
+    ).run(sender=alice.address)
+    scenario += staking_token.update_operators(
+        [
+            sp.variant(
+                "add_operator",
+                sp.record(
+                    owner=dan.address, operator=staking_pool.address, token_id=token_id
+                ),
+            )
+        ]
+    ).run(sender=dan.address)
+    scenario += staking_token.update_operators(
+        [
+            sp.variant(
+                "add_operator",
+                sp.record(
+                    owner=bob.address, operator=staking_pool.address, token_id=token_id
+                ),
+            )
+        ]
+    ).run(sender=bob.address)
+    alice_ledger_key = fa2.LedgerKey.make(0, alice.address)
+    bob_ledger_key = fa2.LedgerKey.make(0, bob.address)
+    dan_ledger_key = fa2.LedgerKey.make(0, dan.address)
+    reward_amount = 1 * Constants.PRECISION_FACTOR
+
+    scenario.h2("Start staking")
+    now = sp.timestamp(0)
+    scenario += staking_pool.deposit(sp.record(token_amount=1 * Constants.PRECISION_FACTOR, stake_id=0)).run(
+        sender=alice, now=now
+    )
+    scenario.p("Alice stake and pay reward")
+    scenario += reward_token.mint(
+        owner=staking_pool.address, token_id=token_id, token_amount=reward_amount
+    )
+
+    scenario.p("Bob stake and pay reward")
+    scenario += staking_pool.deposit(sp.record(token_amount=1 * Constants.PRECISION_FACTOR, stake_id=0)).run(
+        sender=bob, now=now
+    )
+    scenario += reward_token.mint(
+        owner=staking_pool.address, token_id=token_id, token_amount=reward_amount
+    )
+
+    scenario.h2("Alice claim after half-period. Alice has a weight 3/4, and loses 50% of the remaining reward ")
+    now = now.add_seconds(sp.to_int(staking_pool.data.max_release_period // 2))
+    scenario += staking_pool.claim(sp.record(stake_id=1)).run(sender=alice, now=now)
+  
+    alice_reward = reward_amount // 4 * 3
+  
+    scenario.verify_equal(reward_token.data.ledger[alice_ledger_key], alice_reward)
+ 
+    scenario.p("Bob exits after half time. Bob have weitgh 1/4 and 50% of those lost by Alice.")
+    scenario += staking_pool.withdraw(
+        sp.record(stake_id=2)
+    ).run(sender=bob.address, now=now)
+
+    bobs_reward = (reward_amount // 4) + (reward_amount // 16 * 3)
+    scenario.show(reward_token.data.ledger[bob_ledger_key])
+    scenario.verify_equal(reward_token.data.ledger[bob_ledger_key], bobs_reward)
+
+    scenario.h2("Alice withdraw after end stake. Alice has a 0.25% unrelized profit + 50% from losses Bob ")
+    now = now.add_seconds(sp.to_int(staking_pool.data.max_release_period // 2))
+    scenario += staking_pool.withdraw(sp.record(stake_id=1)).run(sender=alice, now=now)
+    alice_reward = reward_amount * 2 - bobs_reward
+
+    scenario.verify_equal(reward_token.data.ledger[alice_ledger_key], alice_reward)
+
+    
+    scenario += reward_token.burn(
+        owner=alice.address,
+        token_id=token_id,
+        token_amount=reward_token.data.ledger[alice_ledger_key],
+    )
+    scenario += reward_token.burn(
+        owner=bob.address,
+        token_id=token_id,
+        token_amount=reward_token.data.ledger[bob_ledger_key],
+    )
+    
+    scenario.h2("Start staking")
+    scenario.p("Alice and Bob stake and pay reward")
+    now = sp.timestamp(0)
+    scenario += staking_pool.deposit(sp.record(token_amount=1 * Constants.PRECISION_FACTOR, stake_id=0)).run(
+        sender=alice, now=now
+    )
+    scenario += staking_pool.deposit(sp.record(token_amount=1 * Constants.PRECISION_FACTOR, stake_id=0)).run(
+        sender=bob, now=now
+    )
+
+    scenario += reward_token.mint(
+        owner=staking_pool.address, token_id=token_id, token_amount=reward_amount * 2
+    )
+
+    now = now.add_seconds(sp.to_int(staking_pool.data.max_release_period // 2))
+ 
+    #now = now.add_seconds(sp.to_int(staking_pool.data.max_release_period // 2))
+    scenario.p("Bob enter in new stake after half-time")
+    scenario.p("Bob stake and pay reward")
+    scenario += staking_pool.deposit(sp.record(token_amount=1 * Constants.PRECISION_FACTOR, stake_id=0)).run(
+        sender=bob, now=now
+    )
+    # scenario += staking_pool.deposit(sp.record(token_amount=1 * Constants.PRECISION_FACTOR, stake_id=0)).run(
+    #     sender=alice, now=now
+    # )
+    scenario += reward_token.mint(
+        owner=staking_pool.address, token_id=token_id, token_amount=reward_amount * 1
+    )
+
+    now = now.add_seconds(sp.to_int(staking_pool.data.max_release_period // 2))
+    scenario.p("Alice withdraw after end period")
+    scenario.p("Alice should get 50% of the first reward and + 1 reward // 3 because Bob increased the stake weight.")
+    scenario.p("stake * (reward / totalDeposit)")
+    #scenario.show(reward_token.data.ledger[alice_ledger_key])
+    scenario += staking_pool.withdraw(
+        sp.record(stake_id=3)
+    ).run(sender=alice.address, now=now)
+    alice_reward = reward_amount + int((reward_amount / (3 * Constants.PRECISION_FACTOR)) * 1 * Constants.PRECISION_FACTOR)
+
+    scenario.verify_equal(reward_token.data.ledger[alice_ledger_key], alice_reward)
+
+    scenario.p("Bob withdraw after end period 1st stake, and receives 100% of 1st reward + 50%  of second reward")
+    scenario += staking_pool.withdraw(
+        sp.record(stake_id=4)
+    ).run(sender=bob.address, now=now)
+    bobs_reward = reward_amount + int((reward_amount / (3 * Constants.PRECISION_FACTOR)) * 1 * Constants.PRECISION_FACTOR)
+    scenario.show(reward_token.data.ledger[bob_ledger_key])
+    scenario.verify_equal(reward_token.data.ledger[bob_ledger_key], bobs_reward)
+
+    scenario.p("Bob withdraw after end period")
+    now = now.add_seconds(sp.to_int(staking_pool.data.max_release_period // 2))
+    scenario += staking_pool.withdraw(
+        sp.record(stake_id=5)
+    ).run(sender=bob.address, now=now)
+    bobs_reward += int((reward_amount / (3 * Constants.PRECISION_FACTOR)) * 1 * Constants.PRECISION_FACTOR)
+    scenario.verify_equal(reward_token.data.ledger[bob_ledger_key], bobs_reward)
